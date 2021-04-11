4055cffb6806d5bea37ead2fd71ba005f30f9f5c
Obi815 <80441506+Obi815@users.noreply.github.com>
2021-04-11 16:53:05 -0700

    Added generator functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 
+console.log(Math.random() * 26);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +16,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generator functions 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+function getRandomSymbol() {
+  var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  return symbols [Math.floor(Math.random()* symbols.length)];
+};
+
+console.log(getRandomSymbol())
